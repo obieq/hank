23a04354f288785d3ef30a92c9d14e16a65a7738
singh-gurpreet <gurpreet.singh@inspopindia.com>
2016-01-21 18:03:18 +0530

    New files

---
+++ Elephant.Hank.Api/src/DataService/DBSchema/CustomIdentity/TokenProvider.cs
@@ -0,0 +1,119 @@
+﻿// ---------------------------------------------------------------------------------------------------
+// <copyright file="TokenProvider.cs" company="Elephant Insurance Services, LLC">
+//     Copyright (c) 2016 All Right Reserved
+// </copyright>
+// <author>Gurpreet Singh</author>
+// <date>2016-01-21</date>
+// <summary>
+//     The TokenProvider class
+// </summary>
+// ---------------------------------------------------------------------------------------------------
+
+namespace Elephant.Hank.DataService.DBSchema.CustomIdentity
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Elephant.Hank.Common.Helper;
+    using Elephant.Hank.Resources.Constants;
+    using Elephant.Hank.Resources.Extensions;
+
+    using Microsoft.AspNet.Identity;
+
+    /// <summary>
+    /// The TokenProvider class
+    /// </summary>
+    public class TokenProvider : IUserTokenProvider<CustomUser, long>
+    {
+        /// <summary>
+        /// The reset password
+        /// </summary>
+        public const string ResetPassword = "ResetPassword";
+
+        /// <summary>
+        /// The email confirmation
+        /// </summary>
+        public const string EmailConfirmation = "Confirmation";
+        
+        /// <summary>
+        /// Generates the asynchronous.
+        /// </summary>
+        /// <param name="purpose">The purpose.</param>
+        /// <param name="manager">The manager.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>Token for reset</returns>
+        public async Task<string> GenerateAsync(string purpose, UserManager<CustomUser, long> manager, CustomUser user)
+        {
+            Guid resetToken = Guid.NewGuid();
+
+            if (purpose.EqualsIgnoreCase(ResetPassword))
+            {
+                user.ResetPasswordToken = resetToken.ToString();
+                user.ResetPasswordSentAt = DateTime.Now;
+                await manager.UpdateAsync(user);
+            }
+            else if (purpose.EqualsIgnoreCase(EmailConfirmation))
+            {
+                user.EmailConfirmationToken = resetToken.ToString();
+                user.EmailConfirmationSentAt = DateTime.Now;
+                await manager.UpdateAsync(user);
+            }
+
+            return resetToken.ToString();
+        }
+
+        /// <summary>
+        /// Determines whether [is valid provider for user asynchronous] [the specified manager].
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>Status as bool</returns>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public Task<bool> IsValidProviderForUserAsync(UserManager<CustomUser, long> manager, CustomUser user)
+        {
+            if (manager == null) throw new ArgumentNullException();
+
+            return Task.FromResult(manager.SupportsUserPassword);
+        }
+
+        /// <summary>
+        /// Notifies the asynchronous.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="manager">The manager.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>Task promise</returns>
+        public Task NotifyAsync(string token, UserManager<CustomUser, long> manager, CustomUser user)
+        {
+            return Task.FromResult(0);
+        }
+
+        /// <summary>
+        /// Validates the asynchronous.
+        /// </summary>
+        /// <param name="purpose">The purpose.</param>
+        /// <param name="token">The token.</param>
+        /// <param name="manager">The manager.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>Token validation</returns>
+        public Task<bool> ValidateAsync(string purpose, string token, UserManager<CustomUser, long> manager, CustomUser user)
+        {
+            bool result = false;
+
+            if (purpose.EqualsIgnoreCase(ResetPassword))
+            {
+                result = user.ResetPasswordSentAt.HasValue
+                             && DateTime.Now.Subtract(user.ResetPasswordSentAt.Value).TotalHours < AppSettings.Get(ConfigConstants.TokenExpirationTimeHours, 6)
+                             && user.ResetPasswordToken == token;
+            }
+            else if (purpose.EqualsIgnoreCase(EmailConfirmation))
+            {
+                result = user.EmailConfirmationSentAt.HasValue
+                             && DateTime.Now.Subtract(user.EmailConfirmationSentAt.Value).TotalHours < AppSettings.Get(ConfigConstants.TokenExpirationTimeHours, 6)
+                             && user.EmailConfirmationToken == token;
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ Elephant.Hank.Api/src/Framework/Extensions/IdentityResultExtensions.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------------------------------------------
+// <copyright file="IdentityResultExtensions.cs" company="Elephant Insurance Services, LLC">
+//     Copyright (c) 2016 All Right Reserved
+// </copyright>
+// <author>Gurpreet Singh</author>
+// <date>2016-01-21</date>
+// <summary>
+//     The IdentityResultExtensions class
+// </summary>
+// ---------------------------------------------------------------------------------------------------
+
+namespace Elephant.Hank.Framework.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Elephant.Hank.Resources.Messages;
+
+    using Microsoft.AspNet.Identity;
+
+    /// <summary>
+    /// The IdentityResultExtensions class
+    /// </summary>
+    public static class IdentityResultExtensions
+    {
+        /// <summary>
+        /// To the messages.
+        /// </summary>
+        /// <param name="src">The source.</param>
+        /// <returns>
+        /// Message objects
+        /// </returns>
+        public static List<Message> ToMessages(this IdentityResult src)
+        {
+            List<Message> result = null;
+
+            if (src != null && !src.Succeeded)
+            {
+                result = src.Errors.Select(error => new Message(error)).ToList();
+            }
+
+            return result;
+        }
+    }
+}
